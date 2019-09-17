9d312cb6caacb01ea067ac396677e25b20d1e1f6
Jacob Gordon <jgordon024@west-mec.org>
2019-09-16 17:37:23 -0700

    error
    Worked on the response text for the httpRequest, however theres an error where the responseText is an empty quote string

--- 02-AJAX/Exercise-LAMP/www/scripts.js
+++ 02-AJAX/Exercise-LAMP/www/scripts.js
@@ -12,7 +12,19 @@
 // global variables
 var selectedCity = "Tucson, AZ";
 var weatherReport;
+var httpRequest = false;
 
+function getRequestObject()
+{
+try {
+    httpRequest = new XMLHttpRequest();
+} catch (requestError) {
+   document.querySelector("p.error").innerHTML = "Forecast not supported by your browser"
+   document.querySelector("p.error").style.display = "block";
+    return false;
+}
+return httpRequest
+}
 function getWeather(evt) {
    var latitude;
    var longitude;
@@ -33,6 +45,19 @@ function getWeather(evt) {
       latitude = 45.5601062;
       longitude = -73.7120832;
    }
+   if(!httpRequest) httpRequest = getRequestObject();
+   httpRequest.abort();
+   httpRequest.open("get","solar.php?" + "lat=" + latitude+"&lng" + longitude, true);
+   httpRequest.send(null);
+   httpRequest.onreadystatechange = fillWeather;
+
+}
+
+function fillWeather(){
+    if(httpRequest.readyState === 4 && httpRequest.status===200){
+        weatherReport = JSON.parse(httpRequest.responseText);    }
+    httpRequest.onreadystatechange = fillWeather;
+
 }
 
 var locations = document.querySelectorAll("section ul li");
