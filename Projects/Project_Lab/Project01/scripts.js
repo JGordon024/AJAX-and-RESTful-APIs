0e7b00d769d0f78ec15e84361a8475e160716024
Jacob Gordon <jgordon024@west-mec.org>
2019-09-18 18:24:41 -0700

    Worked on both projects
    I started on project 1 and made some progress before learning there was an issue in the instructions, which I have now also fixed. I started on project 2 as well, but am currently at an issue where the checkInput function isn't working.

---
+++ Projects/Project_Lab/Project01/scripts.js
@@ -0,0 +1,58 @@
+/*  Project 01_11_02
+
+    Author: 
+    Date:   
+
+    Filename: script.js
+*/
+
+"use strict";
+var httpRequest = false;
+var entry = "IXIC";
+
+function getRequestObject(){
+    try{
+        htptRequest = new XMLHttpRequest()
+    }
+    catch(requestError){
+        return false;
+    }
+    alert(httpRequest);
+    return httpRequest;
+}
+
+if(window.addEventListener){
+    window.addEventListener("load", getRequestObject, false);
+}    else if (window.attachEvent){
+    window.attachEvent("onload",getRequestObject);
+}
+
+function stopSubmission(evt){
+    alert("stopSubmission()");
+    if(evt.preventDefault){
+        evt.preventDefault();
+    }else{
+        evt.returnValue = false;
+    }
+}
+
+var form = document.getElementsByTagName("form")[0];
+if(form.addEventListener){
+    form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", getRequestObject,false);
+}else if( form.attachEvent){
+    form.attachEvent("onsubmit",stopSubmission);
+    window.attachEvent("onload",getRequestObject);
+}
+
+function getQuote(){
+    alert("getQuote()");
+    if(document.getElementsByTagName("input")[0].value){
+        entry = document.getElementsByTagName("input")[0].value
+    }
+    if(!httpRequest){
+        httpRequest = getRequestObject();
+    }
+}
+
+
---
+++ Projects/Project_Lab/www/script.js
@@ -0,0 +1,30 @@
+/*  Project 01_11_04
+
+    Author: 
+    Date:   
+
+    Filename: script.js
+*/
+
+"use strict";
+var httpRequest = false;
+
+var zip = document.getElementById("zip");
+if (zip.addEventListener){
+    zip.addEventListener("keyup", checkInput, false);
+}else if (zip.attachEvent){
+    zip.attachEvent("onkeyup", checkInput);
+}
+
+function checkInput(){
+    alert("checkInput()");
+// var zip = document.getElementById("zip").value;
+// if(zip.length === 5){
+//     getLocation()
+// }
+// else {
+//     document.getElementById("city").value = "";
+//     document.getElementById("state").value = "";
+// }
+}
+
